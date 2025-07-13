cb8c84114a597d442dc51c3fba25eb472854ae8d
Umesh <umesh.nod@gmail.com>
2025-07-13 13:46:40 +0530

    added api

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
       resultDiv.innerHTML = "";
   
       try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://intent-back.onrender.com/predict", {
           method: "POST",
           body: formData
         });
